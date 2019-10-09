6dd8bfcd50ae3b7a28f1d206965a9ebc1a241ae8
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-09 22:12:51 +0100

    Add files via upload
    Categories Table - Inserts

---
+++ inserts_categories.sql
@@ -0,0 +1,40 @@
+-- CATEGORIES TABLE INSERT 
+INSERT INTO categories 
+VALUES      (1, 
+             'BEVERAGES', 
+             'Soft drinks, coffees, teas, beers, and ales'); 
+
+INSERT INTO categories 
+VALUES      (2, 
+             'Condiments', 
+             'Sweet and savory sauces, relishes, spreads, and seasonings s'); 
+
+INSERT INTO categories 
+VALUES      (3, 
+             'Confections', 
+             'Desserts, candies, and sweet breads '); 
+
+INSERT INTO categories 
+VALUES      (4, 
+             'Dairy Products', 
+             'Cheeses'); 
+
+INSERT INTO categories 
+VALUES      (5, 
+             "grains/cereals", 
+             'Breads, crackers, pasta, and cereal'); 
+
+INSERT INTO categories 
+VALUES      (6, 
+             'Meat/Poultry', 
+             'Prepared meats'); 
+
+INSERT INTO categories 
+VALUES      (7, 
+             'Produce', 
+             'Dried fruit and bean curd'); 
+
+INSERT INTO categories 
+VALUES      (8, 
+             'Seafood', 
+             'Seaweed and fish'); 
