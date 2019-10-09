63ed0e1317f115b3cb3e5f27e36b47801a3c537d
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-09 22:10:48 +0100

    Add files via upload
    Shippers Table - Inserts

---
+++ inserts_shippers.sql
@@ -0,0 +1,24 @@
+-- INSERT INTO SHIPPERS TABLE 
+INSERT INTO shippers 
+            (shipperid, 
+             shippername, 
+             phone) 
+VALUES      (1, 
+             'Speedy Express', 
+             503555983); 
+
+INSERT INTO shippers 
+            (shipperid, 
+             shippername, 
+             phone) 
+VALUES      (2, 
+             'United Package', 
+             503555931); 
+
+INSERT INTO shippers 
+            (shipperid, 
+             shippername, 
+             phone) 
+VALUES      (3, 
+             'Federal Shipping', 
+             54455498); 
