9b0a06178f2a2c4fb614b087ad88d4b815c83713
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-09 21:51:00 +0100

    Add files via upload
    Suppliers Table - Insert

---
+++ inserts_suppliers.sql
@@ -0,0 +1,315 @@
+-- INSERT INTO SUPPLIERS
+
+
+INSERT INTO suppliers 
+VALUES      (1, 
+             'Exotic Liquid', 
+             'Charlotte Cooper', 
+             '49 Gilbert St.', 
+             'London', 
+             'EC1 4SD'); 
+
+INSERT INTO suppliers 
+VALUES      (2, 
+             'New Orleans Cajun Delights', 
+             'Shelley Burke', 
+             'P.O. Box 78934', 
+             'New Orleans', 
+             70117, 
+             'USA', 
+             1005554822); 
+
+INSERT INTO suppliers 
+VALUES      (3, 
+             "grandma kelly's homestead", 
+             'Regina Murphy', 
+             '707 Oxford Rd.', 
+             'Ann Arbor', 
+             48104, 
+             'USA', 
+             313135735); 
+
+INSERT INTO suppliers 
+VALUES      (4, 
+             'Tokyo Traders', 
+             'Yoshi Nagase', 
+             '9-8 Sekimai Musashino-shi', 
+             'Tokyo', 
+             100, 
+             'Japan', 
+             0335555033); 
+
+INSERT INTO suppliers 
+VALUES      (5, 
+             "cooperativa de quesos 'las cabras'", 
+             'Antonio del Valle Saavedra', 
+             'Calle del Rosal 4', 
+             'Oviedo', 
+             33337, 
+             'Spain', 
+             985987654); 
+
+INSERT INTO suppliers 
+VALUES      (6, 
+             'Mayumi Ohno     92 Setsuko Chuo-ku', 
+             'Osaka', 
+             '545 Japan', 
+             'Osaka', 
+             545, 
+             'Japan', 
+             064317877); 
+
+INSERT INTO suppliers 
+VALUES      (7, 
+             "pavlova ltd.", 
+             'Ian Devling', 
+             '74 Rose St. Moonie Ponds', 
+             'Melbourne', 
+             3058, 
+             'Australia', 
+             0344442343); 
+
+INSERT INTO suppliers 
+VALUES      (8, 
+             'Specialty Biscuits Ltd.', 
+             "peter wilson", 
+             "29 king's way", 
+             'Manchester', 
+             'M14 3SD', 
+             'UK', 
+             442034598); 
+
+INSERT INTO suppliers 
+VALUES      (9, 
+             "pb kn�ckebr�d ab", 
+             'Lars Peterson', 
+             'Kaloadagatan 13', 
+             'Göteborg', 
+             'S-345 67', 
+             'Sweden', 
+             031987654); 
+
+INSERT INTO suppliers 
+VALUES      (10, 
+             "refrescos americanas ltda'", 
+             'Carlos Diaz', 
+             'Av. das Americanas 12.890', 
+             'São Paulo', 
+             5442, 
+             'Brazil', 
+             115554640); 
+
+INSERT INTO suppliers 
+VALUES      (11, 
+             'Heli Süßwaren GmbH & Co. KG', 
+             'Petra Winkler', 
+             'Tiergartenstraße 5', 
+             'Berlin', 
+             10785, 
+             'Germany', 
+             0109984510); 
+
+INSERT INTO suppliers 
+VALUES      (12, 
+             "plutzer lebensmittelgro�m�rkte ag.", 
+             'Martin Bein', 
+             'Bogenallee 51', 
+             'Frankfurt', 
+             60439, 
+             'Germany', 
+             069992755); 
+
+INSERT INTO suppliers 
+VALUES      (13, 
+             'Nord-Ost-Fisch Handelsgesellschaft mbH', 
+             "sven petersen", 
+             'Frahmredder 112a', 
+             'Cuxhaven', 
+             27478, 
+             'Germany', 
+             47218713); 
+
+INSERT INTO suppliers 
+VALUES      (14, 
+             "formaggi fortini s.r.l.", 
+             'Elio Rossi', 
+             'Viale Dante, 75', 
+             'Ravenna', 
+             48100, 
+             'Italy', 
+             54460323); 
+
+INSERT INTO suppliers 
+VALUES      (15, 
+             "norske meierier", 
+             'Beate Vileid', 
+             'Hatlevegen 5', 
+             'Sandvika', 
+             1320, 
+             'Norway', 
+             02953010); 
+
+INSERT INTO suppliers 
+VALUES      (16, 
+             'Bigfoot Breweries', 
+             'Cheryl Saylor', 
+             '3400 - 8th Avenue Suite 210', 
+             'Bend', 
+             97101, 
+             'USA', 
+             50355931); 
+
+INSERT INTO suppliers 
+VALUES      (17, 
+             "svensk sj�f�da ab", 
+             'Michael Björn', 
+             'Brovallavägen 231', 
+             'Stockholm', 
+             'S-123 45', 
+             'Sweden', 
+             081234567); 
+
+INSERT INTO suppliers 
+VALUES      (18, 
+             'Aux joyeux ecclésiastiques', 
+             "guyl�ne nodier", 
+             "203, rue des francs-bourgeois", 
+             'Paris', 
+             75004, 
+             'France', 
+             49873968); 
+
+INSERT INTO suppliers 
+VALUES      (19, 
+             'New England Seafood Cannery', 
+             'Robb Merchant', 
+             '2100 Paul Revere Blvd.', 
+             'Boston', 
+             02134, 
+             'USA', 
+             031987654); 
+
+INSERT INTO suppliers 
+VALUES      (20, 
+             "leka trading", 
+             'Chandra Leka', 
+             '471 Serangoon Loop', 
+             'Singapore', 
+             0512, 
+             'Singapore', 
+             5558787); 
+
+INSERT INTO suppliers 
+VALUES      (21, 
+             'Lyngbysild', 
+             'Niels Petersen', 
+             'Lyngbysild Fiskebakken 10', 
+             'Lyngby', 
+             2800, 
+             'Denmark', 
+             43844108); 
+
+INSERT INTO suppliers 
+VALUES      (22, 
+             "zaanse snoepfabriek", 
+             'Dirk Luchte', 
+             'Verkoop Rijnweg 22', 
+             'Zaandam', 
+             9999, 
+             'Netherlands', 
+             123451212); 
+
+INSERT INTO suppliers 
+VALUES      (23, 
+             'Karkki Oy', 
+             "anne heikkonen", 
+             'Valtakatu 12', 
+             'Lappeenranta', 
+             'Finland', 
+             'Germany', 
+             47218713); 
+
+INSERT INTO suppliers 
+VALUES      (24, 
+             "g'day mate", 
+             'Wendy Mackenzie', 
+             "170 prince edward parade hunter's hill", 
+             'Sydney', 
+             2042, 
+             'Australia', 
+             25555914); 
+
+INSERT INTO suppliers 
+VALUES      (25, 
+             "ma maison", 
+             'Jean-Guy Lauzon', 
+             '2960 Rue St. Laurent', 
+             'Montréal', 
+             'H1J 1C3', 
+             'Canada', 
+             514559022); 
+
+INSERT INTO suppliers 
+VALUES      (26, 
+             'Pasta Buttini s.r.l.', 
+             'Giovanni Giudici', 
+             'Via dei Gelsomini, 153', 
+             'Salerno', 
+             84100, 
+             'Italy', 
+             089654766); 
+
+INSERT INTO suppliers 
+VALUES      (27, 
+             "escargots nouveaux", 
+             'Marie Delamare', 
+             '22, rue H. Voiron', 
+             'Montceau', 
+             71300, 
+             'France', 
+             85570007); 
+
+INSERT INTO suppliers 
+VALUES      (28, 
+             'Gai pâturage', 
+             'Eliane Noz', 
+             'Bat. B 3, rue des Alpes', 
+             'Annecy', 
+             74000, 
+             'France', 
+             38769806); 
+
+INSERT INTO suppliers 
+VALUES      (29, 
+             "for�ts d'�rables", 
+             'Chantal Goulet', 
+             '148 rue Chasseur', 
+             'Ste-Hyacinthe', 
+             'J2S 7S8', 
+             'Canada', 
+             415552955); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
