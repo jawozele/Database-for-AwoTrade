48c77931afa9c6b6fd026802b4036c045602cebf
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-09 22:00:10 +0100

    Add files via upload
    Employees Table - Inserts

---
+++ inserts_employees.sql
@@ -0,0 +1,102 @@
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Davolio', 
+             'Nancy', 
+             '12-08-1968', 
+             'EmpID1.pic', 
+             "Education includes a ba in psychology from colorado state university. she also completed (the art of the cold call). nancy is a member of 'toastmasters international'. "); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Fuller', 
+             'Andrew', 
+             '1952-02-19', 
+             'EmpID2.pic', 
+"andrew received his bts commercial and a ph.d. in international marketing from the university of dallas. he is fluent in french and italian and reads german. he joined the company as a sales representative, was promoted to sales manager and was then named vice president of sales. andrew is a member of the sales management roundtable, the seattle chamber of commerce, and the pacific rim importers association."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Fuller', 
+             'Andrew', 
+             '1952-02-19', 
+             'EmpID3.pic', 
+"andrew received his bts commercial and a ph.d. in international marketing from the university of dallas. he is fluent in french and italian and reads german. he joined the company as a sales representative, was promoted to sales manager and was then named vice president of sales. andrew is a member of the sales management roundtable, the seattle chamber of commerce, and the pacific rim importers association."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Buchanan', 
+             'Steven', 
+             '1955-03-04', 
+             'EmpID4.pic', 
+"steven buchanan graduated from st. andrews university, scotland, with a bsc degree. upon joining the company as a sales representative, he spent 6 months in an orientation program at the seattle office and then returned to his permanent post in london, where he was promoted to sales manager. mr. buchanan has completed the courses 'successful telemarketing' and 'international sales management'. he is fluent in french."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Suyama', 
+             'Michael', 
+             '1963-07-02', 
+             'EmpID5.pic', 
+"michael is a graduate of sussex university (ma, economics) and the university of california at los angeles (mba, marketing). he has also taken the courses 'multi-cultural selling' and 'time management for the sales professional'. he is fluent in japanese and can read and write french, portuguese, and spanish."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('King', 
+             'Robert', 
+             '1960-05-29', 
+             'EmpID6.pic', 
+"robert king served in the peace corps and traveled extensively before completing his degree in english at the university of michigan and then joining the company. after completing a course entitled 'selling in europe', he was transferred to the london office."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Callahan', 
+             'Laura', 
+             '1958-01-09', 
+             'EmpID8.pic', 
+"laura received a ba in psychology from the university of washington. she has also completed a course in business french. she reads and writes french."
+); 
+
+INSERT INTO employees 
+            (lastname, 
+             firstname, 
+             birthdate, 
+             email, 
+             notes) 
+VALUES      ('Dodsworth', 
+             'Anne', 
+             '1969-07-02', 
+             'EmpID9.pic', 
+"anne has a ba degree in english from st. lawrence college. she is fluent in french and german."
+); 
