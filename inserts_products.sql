71b83e7d17063214029c9b8b0dbcece7db20ea2e
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-10-11 16:37:54 +0100

    Add files via upload
    Products Table - Inserts

---
+++ inserts_products.sql
@@ -0,0 +1,371 @@
+
+
+-- PRODUCT'S INSERT
+
+INSERT INTO PRODUCTS
+VALUES (1, 'Chais', 1, 1, '10 boxes x 20 bags', 18);
+
+
+INSERT INTO PRODUCTS
+VALUES (2, 'Chang', 1, 1, '24 - 12 oz bottles', 19);
+
+INSERT INTO PRODUCTS
+VALUES (3, 'Aniseed Syrup', 1, 2, '12 - 550 ml bottles', 10);
+
+INSERT INTO PRODUCTS
+VALUES (4, "Chef Anton's Cajun Seasoning", 2, 2, '48 - 6 oz jars', 22);
+
+INSERT INTO PRODUCTS
+VALUES (5, "Chef Anton's Gumbo Mix", 2, 2, '36 boxes', 21.35);
+
+INSERT INTO PRODUCTS
+VALUES (6, "Grandma's Boysenberry Spread", 3, 2, '12 - 8 oz jars', 25);
+
+INSERT INTO PRODUCTS
+VALUES (7, "Uncle Bob's Organic Dried Pears", 3, 7, '12 - 1 lb pkgs.', 30);
+
+INSERT INTO PRODUCTS
+VALUES (8, 'Northwoods Cranberry Sauce', 3, 2, '12 - 12 oz jars', 40);
+
+INSERT INTO PRODUCTS
+VALUES (9, 'Mishi Kobe Niku', 4, 6, '18 - 500 g pkgs.', 97);
+
+
+INSERT INTO PRODUCTS
+VALUES (10, 'Ikura', 4, 8, '12 - 200 ml jars', 31);
+
+INSERT INTO PRODUCTS
+VALUES (11, 'Queso Cabrales', 5, 4, '1 kg pkg.', 21);
+
+INSERT INTO PRODUCTS
+VALUES (12, 'Queso Manchego La Pastora', 5, 4, '10 - 500 g pkgs.', 38);
+
+INSERT INTO PRODUCTS
+VALUES (13, 'Konbu', 6, 8, '2 kg box', 6);
+
+INSERT INTO PRODUCTS
+VALUES (14, 'Tofu', 6, 7, '40 - 100 g pkgs.', 23.25);
+
+INSERT INTO PRODUCTS
+VALUES (15, 'Genen Shouyu', 6, 2, '24 - 250 ml bottles', 15.5);
+
+INSERT INTO PRODUCTS
+VALUES (16, 'Pavlova', 7, 3, '32 - 500 g boxes', 17.45);
+
+INSERT INTO PRODUCTS
+VALUES (17, 'Alice Mutton', 7, 6, '20 - 1 kg tins', 39);
+
+
+INSERT INTO PRODUCTS
+VALUES (18, 'Carnarvon Tigers', 7, 8, '16 kg pkg.', 62.5);
+
+INSERT INTO PRODUCTS
+VALUES (19, 'Teatime Chocolate Biscuits', 8, 3, '10 boxes x 12 pieces', 9.2);
+
+INSERT INTO PRODUCTS
+VALUES (20, "Sir Rodney's Marmalade", 8, 3, '30 gift boxes', 81);
+
+INSERT INTO PRODUCTS
+VALUES (21, "Sir Rodney's Scones", 8, 3, '24 pkgs. x 4 pieces', 10);
+
+INSERT INTO PRODUCTS
+VALUES (22, "Gustaf's Knäckebröd", 9, 5, '24 - 500 g pkgs.', 21);
+
+INSERT INTO PRODUCTS
+VALUES (23, 'Tunnbröd', 9, 5, '12 - 250 g pkgs.', 9);
+
+INSERT INTO PRODUCTS
+VALUES (24, 'Guaraná Fantástica', 10, 1, '12 - 355 ml cans', 4.5);
+
+
+INSERT INTO PRODUCTS
+VALUES (25, 'NuNuCa Nuß-Nougat-Creme', 11, 3, '20 - 450 g glasses', 14);
+
+INSERT INTO PRODUCTS
+VALUES (26, "Gumbär Gummibärchen", 11, 3, '100 - 250 g bags', 31.23);
+
+INSERT INTO PRODUCTS
+VALUES (27, "Schoggi Schokolade", 11, 3, '100 - 100 g pieces', 43.9);
+
+INSERT INTO PRODUCTS
+VALUES (28, "Rössle Sauerkraut", 12, 7, '25 - 825 g cans', 45.6);
+
+INSERT INTO PRODUCTS
+VALUES (29, 'Thüringer Rostbratwurst', 12, 6, '50 bags x 30 sausgs.', 123.79);
+
+INSERT INTO PRODUCTS
+VALUES (30, 'Nord-Ost Matjeshering', 13, 8, '10 - 200 g glasses', 25.89);
+
+
+
+
+
+
+
+INSERT INTO products 
+VALUES      (31, 
+             'Gorgonzola Telino', 
+             14, 
+             4, 
+             '10 - 200 g glasses', 
+             12.5); 
+
+INSERT INTO products 
+VALUES      (32, 
+             'Mascarpone Fabioli', 
+             14, 
+             4, 
+             '24 - 200 g pkgs.', 
+             32); 
+
+INSERT INTO products 
+VALUES      (33, 
+             'Geitost', 
+             15, 
+             4, 
+             '500G', 
+             2.5); 
+
+INSERT INTO products 
+VALUES      (34, 
+             "sasquatch ale", 
+             16, 
+             1, 
+             '24 - 12 oz bottles', 
+             14); 
+
+INSERT INTO products 
+VALUES      (35, 
+             "steeleye stout", 
+             16, 
+             1, 
+             '24 - 12 oz bottles', 
+             18); 
+
+INSERT INTO products 
+VALUES      (36, 
+             "inlagd sill", 
+             17, 
+             8, 
+             '24 - 250 g jars', 
+             19); 
+
+INSERT INTO products 
+VALUES      (37, 
+             "gravad lax", 
+             17, 
+             8, 
+             '12 - 500 g pkgs.', 
+             26); 
+
+INSERT INTO products 
+VALUES      (38, 
+             'Côte de Blaye', 
+             18, 
+             1, 
+             '12 - 75 cl bottles', 
+             263.5); 
+
+INSERT INTO products 
+VALUES      (39, 
+             'Chartreuse verte', 
+             18, 
+             1, 
+             '750 cc per bottle', 
+             18); 
+
+INSERT INTO products 
+VALUES      (40, 
+             'Boston Crab Meat', 
+             19, 
+             8, 
+             '24 - 4 oz tins', 
+             18.4); 
+
+INSERT INTO products 
+VALUES      (41, 
+             "jack's new england clam chowder", 
+             19, 
+             8, 
+             '1 kg pkg.', 
+             9.65); 
+
+INSERT INTO products 
+VALUES      (42, 
+             'Singaporean Hokkien Fried Mee', 
+             20, 
+             5, 
+             '32 - 1 kg pkgs.', 
+             14); 
+
+INSERT INTO products 
+VALUES      (43, 
+             'Ipoh Coffee', 
+             20, 
+             1, 
+             '16 - 500 g tins', 
+             46); 
+
+INSERT INTO products 
+VALUES      (44, 
+             'Gula Malacca', 
+             20, 
+             2, 
+             '20 - 2 kg bags', 
+             19.45); 
+
+INSERT INTO products 
+VALUES      (45, 
+             'Røgede sild', 
+             21, 
+             8, 
+             '1k pkg', 
+             9.5); 
+
+INSERT INTO products 
+VALUES      (46, 
+             'Spegesild', 
+             21, 
+             8, 
+             '4 - 450 g glasses', 
+             12); 
+
+INSERT INTO products 
+VALUES      (47, 
+             'Zaanse Koeken', 
+             22, 
+             3, 
+             '10 - 4 oz boxes', 
+             9.5); 
+
+INSERT INTO products 
+VALUES      (48, 
+             'Chocolade', 
+             22, 
+             3, 
+             '10 pkgs.', 
+             12.74); 
+
+INSERT INTO products 
+VALUES      (49, 
+             'Maxilaku', 
+             23, 
+             3, 
+             '24 - 50 g pkgs.', 
+             20); 
+
+INSERT INTO products 
+VALUES      (50, 
+             "valkoinen suklaa", 
+             23, 
+             3, 
+             '12 - 100 g bars', 
+             16.25); 
+
+INSERT INTO products 
+VALUES      (51, 
+             "manjimup dried apples", 
+             24, 
+             7, 
+             '50 - 300 g pkgs.', 
+             53); 
+
+INSERT INTO products 
+VALUES      (52, 
+             "filo mix", 
+             24, 
+             5, 
+             '16 - 2 kg boxes ', 
+             7); 
+
+INSERT INTO products 
+VALUES      (53, 
+             'Perth Pasties', 
+             24, 
+             6, 
+             '48 pieces', 
+             32.8); 
+
+INSERT INTO products 
+VALUES      (54, 
+             'Tourtière', 
+             25, 
+             6, 
+             '16 pies', 
+             7.45); 
+
+INSERT INTO products 
+VALUES      (55, 
+             'Pâté chinois', 
+             25, 
+             6, 
+             '24 boxes x 2 pies', 
+             24); 
+
+INSERT INTO products 
+VALUES      (56, 
+             "gnocchi di nonna alice", 
+             25, 
+             6, 
+             '24 - 250 g pkgs.', 
+             38); 
+
+INSERT INTO products 
+VALUES      (57, 
+             "ravioli angelo", 
+             26, 
+             5, 
+             '24 - 250 g pkgs.', 
+             19.5); 
+
+INSERT INTO products 
+VALUES      (58, 
+             "escargots de bourgogne", 
+             27, 
+             8, 
+             '24 pieces', 
+             13.25); 
+
+INSERT INTO products 
+VALUES      (59, 
+             'Raclette Courdavault', 
+             28, 
+             4, 
+             '5 kg pkg.', 
+             55); 
+
+INSERT INTO products 
+VALUES      (60, 
+             'Camembert Pierrot', 
+             28, 
+             4, 
+             '15 - 300 g rounds', 
+             34); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
